784fc749083f943da64221a8b869ec27314509d8
Wang Lu <cnwanglu@icloud.com>
2023-11-26 19:28:09 +0800

    better check.sh

--- check.sh
+++ check.sh
@@ -1,18 +1,25 @@
-node --version  2&> /dev/null
+#!/bin/bash
+
+node --version  > /dev/null 2>&1
 if [ $? == 127 ]; then
   echo "node is needed"
   exit 1
 fi
 
-cmake --version 2&> /dev/null
+cmake --version > /dev/null 2>&1
 if [ $? == 127 ]; then
   echo "cmake is needed"
   exit 1
 fi
 
-clangd --version 2&> /dev/null
+# Add more dependence check...
+
+ 
+# Check clangd at last, inaccurate result:
+# Clangd binary name might be "clangd-xx(ver)"
+clangd --version > /dev/null 2>&1
 if [ $? == 127 ]; then
-  echo "clangd is needed"
+  echo "Missing clangd, never mind this if binary name is clangd-xx(ver)"
   exit 1
 fi
 
