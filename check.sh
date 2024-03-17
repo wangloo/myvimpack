f301a368f0acee9137557141f03129be19abbd00
Wang Lu <cnwanglu@icloud.com>
2024-03-17 17:20:58 +0800

    增加依赖软件的版本检查

--- check.sh
+++ check.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+vim_version=$(vim --version | head -n 1 | awk '{print $5}')
+if [ "$(echo -ne "8.2.0\n$vim_version" | sort -V | head -n 1)" != "8.2.0" ]; then
+  echo "Vim should > 8.2, now is $vim_version"
+  exit 1
+fi
+
 node --version  > /dev/null 2>&1
 if [ $? == 127 ]; then
   echo "node is needed"
   exit 1
+fi
+node_ver=$(node -v | head -n 1)
+if [ "$(echo -ne "v20.0.0\n$node_ver" | sort -V | head -n 1)" != "v20.0.0" ]; then
+	echo "Node should > 20.0.0, now is $node_ver"
+	exit 1
 fi
 
 cmake --version > /dev/null 2>&1
